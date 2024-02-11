3cdafa0d12ca4f386235aaff004685e87764464d
HakaHakiev <122813219+hhakiev22@users.noreply.github.com>
2024-02-11 12:08:35 +0200

    Add cube details page

--- src/managers/cubeManager.js
+++ src/managers/cubeManager.js
@@ -1,7 +1,26 @@
 const uniqid = require("uniqid");
-const cubes = [];
+const cubes = [
+  {
+    id: "3psclglshbp2r8",
+    name: "Mirror Cube",
+    description: "A real cute mirror cube",
+    imageUrl:
+      "https://images-na.ssl-images-amazon.com/images/I/61izOzq%2BBAL._SY355_.jpg",
+    difficultyLevel: 4,
+  },
+  {
+    id: "3p2clglshgp2r8",
+    name: "Rubic Classic",
+    description: "Evergreen",
+    imageUrl:
+      "https://images-na.ssl-images-amazon.com/images/I/61HpQqVQ37L._SY355_.jpg",
+    difficultyLevel: 5,
+  },
+];
 
 exports.getAll = () => cubes.slice();
+
+exports.getOne = (cubeId) => cubes.find((x) => x.id == cubeId); // get data to details page
 
 exports.create = (cubeData) => {
   const newCube = {
