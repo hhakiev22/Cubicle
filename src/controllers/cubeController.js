d7c21ab2e7cae2862f1a053931a3927df6241035
HakaHakiev <122813219+hhakiev22@users.noreply.github.com>
2024-02-11 23:23:08 +0200

    Show cube and accessories on attach page

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 
 const cubeManager = require("../managers/cubeManager");
+const accessoryManager = require("../managers/accessoryManager");
 
 router.get("/create", (req, res) => {
   // console.log(cubeManager.getAll());
@@ -30,8 +31,11 @@ router.get("/:cubeId/details", async (req, res) => {
   res.render("details", { cube });
 });
 
-router.get("/:cubeId/attach-accessory", (req, res) => {
-  res.render("accessory/attach");
+router.get("/:cubeId/attach-accessory", async (req, res) => {
+  const cube = await cubeManager.getOne(req.params.cubeId).lean();
+  const accessories = await accessoryManager.getAll().lean(); // get data from accessories
+
+  res.render("accessory/attach", { cube, accessories }); // send data to attach.hbs
 });
 
 module.exports = router;
--- src/managers/accessoryManager.js
+++ src/managers/accessoryManager.js
@@ -1,3 +1,5 @@
 const Accessory = require("../models/Accessories");
 
+exports.getAll = () => Accessory.find(); // get all data from db for accessories
+
 exports.create = (accessoryData) => Accessory.create(accessoryData);
