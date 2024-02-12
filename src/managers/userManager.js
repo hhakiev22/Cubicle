ca456724b71ae5e28471dc907c0eb100a24d6173
HakaHakiev <122813219+hhakiev22@users.noreply.github.com>
2024-02-13 00:08:33 +0200

    User login

--- src/managers/userManager.js
+++ src/managers/userManager.js
@@ -1,3 +1,23 @@
+const bcrypt = require("bcrypt");
+
 const User = require("../models/User");
 
 exports.register = (userData) => User.create(userData);
+
+exports.login = async (username, password) => {
+  // TODO: find user
+  const user = await User.findOne({ username });
+
+  // TODO: validate username
+  if (!user) {
+    throw new Error("Cannot find username or password");
+  }
+
+  // TODO: validate password
+  const isValid = await bcrypt.compare(password, user.password);
+  if (!isValid) {
+    throw new Error("Cannot find username or password");
+  }
+
+  return user;
+};
