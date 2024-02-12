56291b99559a3489a71d7b3619cc3dea3d87cdd7
HakaHakiev <122813219+hhakiev22@users.noreply.github.com>
2024-02-12 23:48:22 +0200

    Hash password before save

--- src/models/User.js
+++ src/models/User.js
@@ -1,14 +1,23 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
-const userSchema = {
+const userSchema = new mongoose.Schema({
   username: String,
-  password: String,
-};
+  password: {
+    type: String,
+  },
+});
 
 userSchema.virtual("repeatPassword").set(function (value) {
   if (value !== this.password) {
     throw new mongoose.MongooseError("Passwords missmatch!");
   }
+});
+
+userSchema.pre("save", async function () {
+  const hash = await bcrypt.hash(this.password, 10);
+
+  this.password = hash;
 });
 
 const User = mongoose.model("User", userSchema);
